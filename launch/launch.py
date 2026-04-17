d1167c3c0718ac8ed2d6d46cfdf8352b95720312
Lucas Woolfie <lucaswoolfie@gmail.com>
2026-04-17 12:30:40 +0800

    Create launch.py

---
+++ launch/launch.py
@@ -0,0 +1,18 @@
+EXAMPLE:
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ariaNode',
+            executable='ariaNode',
+            arguments=['-remoteHost', '192.168.1.32'],
+            output='screen'
+        ),
+        Node(
+            package='pioneer_autonomy',
+            executable='waypoint_controller',
+            output='screen'
+        ),
+    ])
