c9b24e830a34922b05a1cab1b367a2d9081e8136
rosarahmani1 <rahmani.rosa@gmail.com>
2026-04-28 18:12:06 +0800

    Update setup.py

--- ros2_ws/src/pioneer_nav/setup.py
+++ ros2_ws/src/pioneer_nav/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'distbug_controller = pioneer_nav.distbug_controller:main',
+            'lidarstop = pioneer_nav.lidarstop:main',
             'ps4_basic_drive = pioneer_nav.ps4_basic_drive:main',
             'mission_manager = pioneer_nav.mission_manager:main',
             'colour_detector = pioneer_nav.colour_detector_node:main',
