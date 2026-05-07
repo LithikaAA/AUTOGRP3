80ac0037f77bb08e3dbd3d748db954e7c12a2e35
Lucas Woolfie <lucaswoolfie@gmail.com>
2026-05-07 13:50:46 +0800

    Update letter_detector_node.py

--- ros2_ws/src/pioneer_nav/pioneer_nav/letter_detector_node.py
+++ ros2_ws/src/pioneer_nav/pioneer_nav/letter_detector_node.py
@@ -173,7 +173,8 @@ class LetterDetectorNode(Node):
         letter = self.extract_letter(region)
         if letter.size == 0:
             return
-
+            
+        cv2.imwrite('/tmp/letter_input.png', letter)
         name, confidence = self.classify(letter)
 
         if confidence > self.conf_thresh:
